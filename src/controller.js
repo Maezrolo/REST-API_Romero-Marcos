dcbdca4b9963d25f0f0a480fcdb7bc0437a622af
Maezrolo <marcosuse007@gmail.com>
2023-10-24 10:09:28 -0300

    actualizar los libros

--- src/controller.js
+++ src/controller.js
@@ -1,11 +1,13 @@
 import {pool} from './database.js';
 class LibrosController{
 
+    //ver todos los libros
     async getAll(req, res) {
         const [result] = await pool.query('SELECT * FROM libros');
         res.json(result);
     }
 
+    //buscar un libro por id
     async getOne(req, res) {
         try{
                 const libro = req.body;
@@ -21,12 +23,45 @@ class LibrosController{
         }
     }
 
+    //agregar un libro
     async add (req, res){
         const libros=req.body;
         const [result] = await pool.query(`INSERT INTO libros ( Nombre, Autor, Categoria ,año,ISBN) VALUES (?, ?, ?,?,?)`,
         [libros.Nombre, libros.Autor,libros.Categoria, libros.año, libros.ISBN]);
         res.json({"Id insertado": result.insertId});
     }
+    
+    async update(req, res) {
+        try{
+                const libro = req.body;
+                const id_libro = parseInt(libro.ISBN);
+                const [result] = await pool.query(`select * from libros where ISBN=?`, [id_libro]);
+                if (result.length === 1) {
+                    const updateResult = await pool.query(`UPDATE libros SET Nombre = ?, Autor = ?, Categoria = ?, año = ?, ISBN = ? WHERE id_libro = ?`,
+                    [
+                        libro.Nombre,
+                        libro.Autor,
+                        libro.Categoria,
+                        libro.año,
+                        libro.ISBN,
+                        id_libro
+                    ]
+                );
+                if (updateResult.affectedRows === 1) {
+                    // El libro se actualizó con éxito.
+                    res.json({ message: 'Libro actualizado con éxito' });
+                } else {
+                    // No se pudo actualizar el libro.
+                    res.status(500).json({ error: 'Error al actualizar el libro' });
+                }
+                }else{
+                    res.json({"Error": "No se ha encontrado un libro con la Id especificada"});
+                }
+            }catch(e) {
+                console.log(e);
+        }
+    }
+    
 }
 
 export const libro = new LibrosController();
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { libro } from './controller.js';
 
 export const router = Router()
 
-router.get('/libros', libro.getAll);
-router.get('/libro', libro.getOne);
-router.post('/libro', libro.add);
+router.get('/libros', libro.getAll);//ver todos los libros
+router.get('/libro', libro.getOne);//buscar un libros por id
+router.post('/libro', libro.add);////agregar un libro
+router.put('/libro',libro.update);
